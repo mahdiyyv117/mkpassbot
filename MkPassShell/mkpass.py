7d7dd53ef9a6a9360ae59da2149a1acb473d9d85
mahdiyyv117 <iranpro117@gmail.com>
2022-02-11 19:28:30 +0000

    fix bug

---
+++ MkPassShell/mkpass.py
@@ -0,0 +1,12 @@
+from ntpath import join
+import string
+import random
+
+numch = int(input("number of characters "))
+character = string.ascii_letters + string.digits + string.punctuation
+password = ""
+
+for i in range(numch):
+    password += random.choice(character)
+
+print("your password", password, sep="\n")

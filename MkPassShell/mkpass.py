877454203b931ac35738082fa4c8b3a4259f60ac
mahdiyyv117 <iranpro117@gmail.com>
2022-02-14 11:46:17 +0000

    fixed bugs

--- MkPassShell/mkpass.py
+++ MkPassShell/mkpass.py
@@ -1,13 +1,20 @@
 import string
 import random
 
-numch = int(input("number of characters "))
+# all possible scenarios for string
 character = string.ascii_letters + string.digits + string.punctuation
 password = ""
 
-if numch < 8:
-    print("please enter a larger number")
-else:
-    for i in range(numch):
-        password += random.choice(character)
-    print("your password", password, sep="\n")
+try:
+    # number of password characters
+    numch = int(input("number of characters "))
+    if numch < 8:
+        # for a good password
+        print("please enter a larger number")
+    else:
+        # choice a random password
+        for i in range(numch):
+            password += random.choice(character)
+        print("your password", password, sep="\n")
+except ValueError:
+    print("please enter the correct value")
